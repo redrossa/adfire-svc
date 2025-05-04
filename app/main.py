dabafefb1801ae1b682555f9d0926246caab8b16
Reno Raksi <adriano.raksi@gmail.com>
2025-05-04 16:07:26 -0500

    Add dev frontend origin to CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth import AuthDep
 from app.db.models import Users
 from app.routers import transactions, accounts
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(transactions.routers)
 
