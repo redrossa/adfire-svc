ae81da68114469ede7c87152792fb86a5151e390
Reno Raksi <adriano.raksi@gmail.com>
2025-05-04 16:09:19 -0500

    Remove headers

--- app/headers/models.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class CommonHeaders(BaseModel):
-    x_session_token: str
--- app/headers/utils.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-
-from fastapi import Header
-
-from app.headers.models import CommonHeaders
-
-Headers = Annotated[CommonHeaders, Header()]
