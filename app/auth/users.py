9b6e36c4fb0723fc5375aadccc015e7c951fe241
Reno Raksi <adriano.raksi@gmail.com>
2025-05-04 16:08:31 -0500

    Updated auth dep to use cookie

--- app/auth/users.py
+++ app/auth/users.py
@@ -1,16 +1,18 @@
-from fastapi import HTTPException, Depends
+from typing import Annotated
+
+from fastapi import HTTPException, Depends, Cookie
 from sqlmodel import select
 
+from app.cookies import Cookies
 from app.db import SessionDep
 from app.db.models import Sessions, Users
-from app.headers import Headers
 
 
-def get_user(headers: Headers, session: SessionDep):
+def get_user(cookies: Annotated[Cookies, Cookie()], session: SessionDep):
     result = session.exec(
         select(Sessions, Users)
         .join(Users)
-        .where(Sessions.sessionToken == headers.x_session_token)
+        .where(Sessions.sessionToken == cookies.session_token)
     ).first()
     if not result:
         raise HTTPException(status_code=404, detail='Invalid session token')
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,2 @@
 from .utils import SessionDep
+from .models import Users, Sessions
