18550bebb4772cb0419ee499eb1eda214a236e0f
Reno Raksi <adriano.raksi@gmail.com>
2025-05-09 09:55:14 -0500

    Refactor /accounts endpoints

--- app/routers/accounts.py
+++ app/routers/accounts.py
@@ -1,11 +1,12 @@
-from typing import List, Annotated
+from fastapi import APIRouter, HTTPException
+from pydantic import Field
+from sqlalchemy import func
+from sqlmodel import select
+from starlette.status import HTTP_404_NOT_FOUND
 
-from annotated_types import MinLen
-from fastapi import APIRouter
-from pydantic import BaseModel
-
-from app.auth import AuthDep
-from app.db import Users
+from app.deps import DBSessionDep, AuthUserDep
+from app.models import core
+from app.models.api_base import ApiBase
 
 routers = APIRouter(
     prefix='/accounts',
@@ -13,52 +14,132 @@ routers = APIRouter(
 )
 
 
-class User(BaseModel):
-    name: Annotated[str, MinLen(1)]
-    mask: Annotated[str, MinLen(1)]
+class AccountUser(ApiBase):
+    name: str = Field(min_length=1)
+    mask: str = Field(min_length=1)
 
 
-class Account(BaseModel):
-    name: Annotated[str, MinLen(1)]
-    users: Annotated[List[User], MinLen(1)]
+class AccountRequest(ApiBase):
+    name: str = Field(min_length=1)
+    users: list[AccountUser] = Field(min_length=1)
 
 
-@routers.get('/')
-async def get_accounts():
-    """Returns all accounts belonging to user"""
-    return [
-        {
-            'id': 'cma5vgtvz00020ck0757f1rri',
-            'name': 'Chase Total Checking',
-            'usersCount': 1
-        },
-        {
-            'id': 'cma5vgtvz00030ck03pd4chhn',
-            'name': 'Chase Freedom Unlimited',
-            'usersCount': 1
-        },
-        {
-            'id': 'cma5vgtvz00040ck0ap7t2qho',
-            'name': 'Amex Gold',
-            'usersCount': 2
-        }
-    ]
+class Account(AccountRequest):
+    id: str = Field(min_length=1)
 
 
-@routers.get('/{id}')
-async def get_account(id: str):
-    """Returns an account by id belonging to user"""
-    return {
-        'id': 'cma5vgtvz00040ck0ap7t2qho',
-        'name': 'Amex Gold',
-        'users': [
-            {'name': 'John Doe', 'mask': '0001'},
-            {'name': 'Jane Doe', 'mask': '0002'}
-        ]
-    }
+class AccountSynopsis(ApiBase):
+    id: str = Field(min_length=1)
+    name: str = Field(min_length=1)
+    users_count: int
 
 
-@routers.post('/')
-async def create_account(account: Account, user: Users = AuthDep):
+@routers.get('/', response_model=list[AccountSynopsis])
+async def get_accounts(db: DBSessionDep, authUser: AuthUserDep):
+    """Returns all account synopses belonging to user"""
+    result = db.exec(
+        select(core.Account.pub_id, core.Account.name, func.count(core.AccountUser.account_id))
+        .join(core.AccountUser)
+        .where(core.Account.owner_id == authUser.id)
+        .group_by(core.Account.id)
+    ).all()
+    return [AccountSynopsis(
+        id=id,
+        name=name,
+        users_count=users_count
+    ) for id, name, users_count in result]
+
+
+@routers.get('/{id}', response_model=Account)
+async def get_account(id: str, db: DBSessionDep, authUser: AuthUserDep):
+    """Returns an account by {id} belonging to user"""
+    account = db.exec(
+        select(core.Account)
+        .where(core.Account.owner_id == authUser.id, core.Account.pub_id == id)
+    ).one_or_none()
+
+    if not account:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f'Account "{id}" not found')
+
+    return Account(
+        id=account.pub_id,
+        name=account.name,
+        users=[AccountUser(name=u.name, mask=u.mask) for u in account.users]
+    )
+
+
+@routers.post('/', response_model=Account)
+async def create_account(accountRequest: AccountRequest, db: DBSessionDep, authUser: AuthUserDep):
     """Creates a new account for this user"""
-    return account
+    users = [core.AccountUser(name=u.name, mask=u.mask) for u in accountRequest.users]
+    account = core.Account(name=accountRequest.name, users=users, owner_id=authUser.id)
+
+    db.add(account)
+    db.commit()
+
+    return Account(
+        id=str(account.pub_id),
+        name=account.name,
+        users=[AccountUser(name=u.name, mask=u.mask) for u in account.users]
+    )
+
+
+@routers.put('/{id}', response_model=Account)
+async def upsert_account(id: str, accountRequest: AccountRequest, db: DBSessionDep, authUser: AuthUserDep):
+    """
+    Replaces the account by {id} with {accountRequest} if it exists,
+    otherwise inserts a new account by {id}
+    """
+    account = db.exec(
+        select(core.Account)
+        .where(core.Account.owner_id == authUser.id, core.Account.pub_id == id)
+    ).one_or_none()
+
+    if not account:
+        # account doesn't exist => create it
+        return create_account(accountRequest, db, authUser)
+
+    # account exists => replace it
+    existing_by_mask = {u.mask: u for u in account.users}
+    incoming_masks = {u.mask for u in accountRequest.users}
+
+    for u in accountRequest.users:
+        mask = u.mask
+        name = u.name
+        if mask in existing_by_mask:
+            existing_by_mask[mask].name = name
+        else:
+            db.add(core.AccountUser(
+                name=name,
+                mask=mask,
+                account_id=account.id
+            ))
+
+    for mask, u in existing_by_mask.items():
+        if mask not in incoming_masks:
+            db.delete(u)
+
+    db.add(account)
+    db.commit()
+    db.refresh(account)
+
+    return Account(
+        id=account.pub_id,
+        name=account.name,
+        users=[AccountUser(name=u.name, mask=u.mask) for u in account.users],
+    )
+
+
+@routers.delete('/{id}')
+async def delete_account(id: str, db: DBSessionDep, authUser: AuthUserDep):
+    """Removes the account by {id} belonging to the user"""
+    account = db.exec(
+        select(core.Account)
+        .where(core.Account.owner_id == authUser.id, core.Account.pub_id == id)
+    ).one_or_none()
+
+    if not account:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f'Account "{id}" not found')
+
+    db.delete(account)
+    db.commit()
