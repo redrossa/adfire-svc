7baa5b06e216859d56678b8f873e0bb374d7513e
Reno Raksi <adriano.raksi@gmail.com>
2025-05-12 21:15:10 -0500

    Forbid extraneous properties

--- app/models/api_base.py
+++ app/models/api_base.py
@@ -3,4 +3,4 @@ from pydantic.alias_generators import to_camel
 
 
 class ApiBase(BaseModel):
-    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True, from_attributes=True, extra='forbid')
